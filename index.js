e15328919474a80b7d9f3757904585d64c36efaa
micromousegp <1parentgab@hdsb.ca>
2024-09-13 12:30:00 -0400

    calculator micromousegp08

--- index.js
+++ index.js
@@ -2,35 +2,39 @@ console.log ("welcome to a calculator I think")
 console.log ("please enter your first number")
 console.log ("...")
 
-// set memery, call memery
 
-let num2
-let ans
 
 let num1 = prompt ("please enter your first number")
-
-if (typeof(num1) === number) {
-	// value is numeric
+if (typeof(Number(num1)) === "number") { 
+	console.log (num1)
 } else {
-    console.log ("fuck")
+    console.log ("invalide number")
 }
 
-
 let operation = prompt ("please enter your operation for  calculation")
-
 if (operation == "-" || operation == "+" || operation == "*" || operation == "/") {
     console.log (operation)
 } else {
     console.log ("invalide operation")
 }
 
-
-
-
+let num2 = prompt ("please enter your first number")
+if (typeof(Number(num2)) === "number") {
+	console.log (num2)
+} else {
+    console.log ("invalide number")
+}
 
 if (operation == "+") {
-    num1 + num2
+    let ans = (Number(num1) + Number(num2))
+    console.log (ans)
 } else if (operation == "-") {
-    num1 - num2
+    let ans = (num1 - num2)
+    console.log (ans)
+} else if (operation == "*") {
+    let ans = (num1 * num2)
+    console.log (ans)
+} else if (operation == "/") {
+    let ans = (num1 / num2)
+    console.log (ans)
 }
-3
