e1cdbd1283abfd3d2bf4e8d09172ba05176aadfc
micromousegp <1parentgab@hdsb.ca>
2024-09-16 12:30:51 -0400

    calculator micromosegp08

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function runProgram () {
+// start text
 console.log ("welcome to a calculator I think")
 console.log ("please enter your first number")
 console.log ("...")
 
 
-
+// full function for the first number
 let num1 = prompt ("please enter your first number")
-if (typeof(Number(num1)) === "number") {
-	console.log (num1)
+if (isNaN(num1)) { // checking for number
+	console.log ("invalide number")
 } else {
-    console.log ("invalide number")
+    console.log (num1)
 }
 
+// function for operation
 let operation = prompt ("please enter your operation for  calculation")
 if (operation == "-" || operation == "+" || operation == "*" || operation == "/") {
     console.log (operation)
@@ -19,15 +21,17 @@ if (operation == "-" || operation == "+" || operation == "*" || operation == "/"
     console.log ("invalide operation")
 }
 
+// full function for the second number
 let num2 = prompt ("please enter your second number")
-if (typeof(Number(num2)) === "number") {
-	console.log (num2)
-} else {
+if (isNaN(num2)) { // checking for number
     console.log ("invalide number")
+} else {
+    console.log (num2)
 }
 
+
 if (operation == "+") {
-    let ans = (num1) + (num2)
+    let ans = (Number(num2)) + (Number(num1))
     console.log (ans)
 } else if (operation == "-") {
     let ans = (num1 - num2)
@@ -39,5 +43,6 @@ if (operation == "+") {
     let ans = (num1 / num2)
     console.log (ans)
 }
+runProgram ()
 }
 runProgram ()
